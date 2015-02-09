e479b92c72445d6e7ee85805ae509ae5f9f9c472
tatsu <tatsu@vagrant-centos65.vagrantup.com>
2015-02-09 07:21:50 +0000

    json_set

--- app/controllers/ajax_test_controller.rb
+++ app/controllers/ajax_test_controller.rb
@@ -1,30 +1,16 @@
 class AjaxTestController < ApplicationController
   def index
-    @greeting = params[:greeting]
-    greeting1 = params[:greeting1].to_s
-    greeting2 = params[:greeting2].to_s
-    greeting3 = params[:greeting3].to_s
 
-    c1_ary = greeting1.split(//)
-    c2_ary = greeting2.split(//)
-    c3_ary = greeting3.split(//)
+    @data2 = {
+      title: params[:greeting].to_s,
+      description: "test description2",
 
-#c1_ary = Array["あ", "か", "さ", "た", "な", "は", "ま", "や", "ら", "わ"]
-#c2_ary = Array["あ", "か", "さ", "た", "な", "は", "ま", "や", "ら", "わ"]
-#c3_ary = Array["ろ"]
-
-@kekka = []
-@kekka2 = ""
-
-for c1 in c1_ary do
-  for c2 in c2_ary do
-    for c3 in c3_ary do
-      @kekka.push( c1 + c2 + c3 )
-      @kekka2 = @kekka2 + c1 + c2 + c3 + "<br>"
-    end
-  end
-end
-
+      list: [
+        { name: 'name-1', score: 210 },
+        { name: 'name-2', score: 220 },
+        { name: 'name-3', score: 230 }
+      ]
+    };
 
     render
   end
