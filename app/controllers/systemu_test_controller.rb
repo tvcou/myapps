6db8241a5fc5e56f2ff0cf7905ed2222d09a3c56
tvcou <tvcou.zyx77@gmail.com>
2015-02-11 11:40:14 +0900

    mod__systemu_test

--- app/controllers/systemu_test_controller.rb
+++ app/controllers/systemu_test_controller.rb
@@ -1,5 +1,10 @@
+require 'systemu'
+
 class SystemuTestController < ApplicationController
   def index
+    cmd = %q( date )
+    status, stdout, stderr = systemu cmd
+    @out = stdout
     render
   end
 end
